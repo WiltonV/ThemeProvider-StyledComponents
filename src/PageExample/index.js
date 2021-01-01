aa4b559ca635e6c6ca4d1e45375e5baa294ae634
Wilton Vieira <wlv.wilton@gmail.com>
2021-01-01 10:45:55 -0300

    M - Using StyledComponents to define Theme application

---
+++ src/PageExample/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Title } from './styles';
+
+const PageExample = () => {
+  return (
+    <Container>
+      <Title>Texto de exemplo</Title>
+    </Container>
+  );
+}
+
+export default PageExample;
